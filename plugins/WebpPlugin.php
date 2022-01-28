2194ab6d5548f15268fa73282d2f022ab169c689
elec3c <54912007+elec3c@users.noreply.github.com>
2022-01-28 15:04:14 +0300

    Проверка имени файла на кириллицу и пробелы

--- plugins/WebpPlugin.php
+++ plugins/WebpPlugin.php
@@ -1,5 +1,26 @@
 <?php
-if ($modx->event->name =='OnFileManagerUpload') {
+if ($modx->event->name =='OnFileManagerUpload' && in_array($files['file']['type'], array("image/png","image/jpeg"))) {
+    $modx->log(1, print_r($array, "<pre>".print_r($files)."</pre>"),'HTML');
+    
+    if(preg_match("/[А-Яа-я]/", $files['file']['name']) or stristr($files['file']['name'], " ")){
+        $newName = $modx->runSnippet('translit',array(
+            'value' => $files['file']['name']
+        ));
+        $modx->log(1, "новое имя ".$newName,'HTML');
+        for($i=1;$i!=-1;$i++){
+            $modx->log(1, "новое имя ".$newName,'HTML');
+            if (!file_exists($modx->config['base_path'].$directory.$newName)){
+                $modx->log(1, $modx->config['base_path'].$directory.$newName,'HTML');
+                rename($modx->config['base_path'].$directory.$files['file']['name'], $modx->config['base_path'].$directory.$newName);
+                break;
+            }
+            else {
+                $newName = "copy_".$newName;
+            }
+        }
+        
+    }
+    $files['file']['name']=$newName ? $newName : $files['file']['name']; 
 	$modx->runSnippet('WebPConverter',array(
         'url' => $directory.$files['file']['name']
      ));
